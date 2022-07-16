3a008adf8da6dc52b02258632a12e327478abfaf
aki matsuno <matsuaki4@gmail.com>
2022-07-16 11:57:57 +0900

    記事を表示する画面のMockを作成

---
+++ lib/article.dart
@@ -0,0 +1,10 @@
+// ignore_for_file: file_names
+
+class Article {
+  final String publishDate;
+  final String title;
+  final String description;
+  final String link;
+
+  Article(this.publishDate, this.title, this.description, this.link);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:technewsconciergemobile2/Article.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,58 +11,88 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Scaffold(
+          appBar: AppBar(),
+          body: _contents(),
+          floatingActionButton: _floatingActionButton()),
     );
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+  Widget _contents() {
+    Article article = Article(
+        '2022/7/16(土) 11:00',
+        'ワンキャリアのCTOが登壇するオンラインセミナー「CTOが特別解説！『サービススケールで直面した組織課題とマイクロサービスアーキテクチャ戦略』」が7月21日に開催',
+        '翔泳社とCodeZineが主催する開発者向けイベント「Developers Summit 2022&amp;nbsp;Summer（デブサミ2022夏）」が7月21日にオンラインで開催されます。今年は「アジャイル・DevOps時代のプロダクトとエンジニア組織を支える、Developer Productivityへの道」をテーマとした34セッションを提供します。今回は「開発プロセス」のカテゴリからいくつかのセッションをご紹介します。',
+        'http://codezine.jp/article/detail/16206');
+    return Column(children: [
+      Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Align(
+          alignment: Alignment.topLeft,
+          child: Text(article.publishDate),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      const SizedBox(
+        height: 12,
       ),
-    );
+      Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Text(
+          article.title,
+        ),
+      ),
+      const SizedBox(
+        height: 12,
+      ),
+      Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Text(
+          article.description,
+        ),
+      ),
+      const SizedBox(
+        height: 12,
+      ),
+      TextButton(onPressed: () => {}, child: const Text("記事を読む")),
+      const SizedBox(
+        height: 12,
+      ),
+      Padding(
+        padding: const EdgeInsets.all(20),
+        child: Align(
+          alignment: Alignment.bottomRight,
+          child: IconButton(
+            icon: const Icon(Icons.favorite),
+            onPressed: () => {},
+          ),
+        ),
+      )
+    ]);
+  }
+
+  Widget _floatingActionButton() {
+    return SafeArea(
+        child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: const [
+                Icon(
+                  Icons.home,
+                  size: 32,
+                ),
+                Icon(
+                  Icons.favorite,
+                  size: 32,
+                ),
+                Icon(
+                  Icons.menu,
+                  size: 32,
+                )
+              ],
+            )));
   }
 }

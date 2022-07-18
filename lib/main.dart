3571393d3da1d9ba0c3c51de202309e248fb2433
aki matsuno <matsuaki4@gmail.com>
2022-07-18 23:09:01 +0900

    レイアウトの修正

--- lib/main.dart
+++ lib/main.dart
@@ -27,72 +27,76 @@ class MyApp extends StatelessWidget {
         'ワンキャリアのCTOが登壇するオンラインセミナー「CTOが特別解説！『サービススケールで直面した組織課題とマイクロサービスアーキテクチャ戦略』」が7月21日に開催',
         '翔泳社とCodeZineが主催する開発者向けイベント「Developers Summit 2022&amp;nbsp;Summer（デブサミ2022夏）」が7月21日にオンラインで開催されます。今年は「アジャイル・DevOps時代のプロダクトとエンジニア組織を支える、Developer Productivityへの道」をテーマとした34セッションを提供します。今回は「開発プロセス」のカテゴリからいくつかのセッションをご紹介します。',
         'http://codezine.jp/article/detail/16206');
-    return Column(children: [
-      Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Align(
-          alignment: Alignment.topLeft,
-          child: Text(article.publishDate),
-        ),
-      ),
-      const SizedBox(
-        height: 12,
-      ),
-      Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Text(
-          article.title,
-        ),
-      ),
-      const SizedBox(
-        height: 12,
-      ),
-      Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Text(
-          article.description,
-        ),
-      ),
-      const SizedBox(
-        height: 12,
-      ),
-      TextButton(onPressed: () => {}, child: const Text("記事を読む")),
-      const SizedBox(
-        height: 12,
-      ),
-      Padding(
-        padding: const EdgeInsets.all(20),
-        child: Align(
-          alignment: Alignment.bottomRight,
-          child: IconButton(
-            icon: const Icon(Icons.favorite),
-            onPressed: () => {},
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(16),
+          child: Align(
+            alignment: Alignment.topLeft,
+            child: Text(article.publishDate),
           ),
         ),
-      )
-    ]);
+        const SizedBox(
+          height: 12,
+        ),
+        Padding(
+          padding: const EdgeInsets.all(16),
+          child: Text(
+            article.title,
+          ),
+        ),
+        const SizedBox(
+          height: 12,
+        ),
+        Padding(
+          padding: const EdgeInsets.all(16),
+          child: Text(
+            article.description,
+          ),
+        ),
+        const SizedBox(
+          height: 12,
+        ),
+        TextButton(onPressed: () => {}, child: const Text("記事を読む")),
+        const SizedBox(
+          height: 12,
+        ),
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: Align(
+            alignment: Alignment.bottomRight,
+            child: IconButton(
+              icon: const Icon(Icons.favorite),
+              onPressed: () => {},
+            ),
+          ),
+        )
+      ],
+    );
   }
 
   Widget _floatingActionButton() {
     return SafeArea(
-        child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: const [
-                Icon(
-                  Icons.home,
-                  size: 32,
-                ),
-                Icon(
-                  Icons.favorite,
-                  size: 32,
-                ),
-                Icon(
-                  Icons.menu,
-                  size: 32,
-                )
-              ],
-            )));
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: const [
+            Icon(
+              Icons.home,
+              size: 32,
+            ),
+            Icon(
+              Icons.favorite,
+              size: 32,
+            ),
+            Icon(
+              Icons.menu,
+              size: 32,
+            )
+          ],
+        ),
+      ),
+    );
   }
 }

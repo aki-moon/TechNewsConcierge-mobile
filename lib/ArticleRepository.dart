98b459107ea8e61f1931ae918a5a4d920e9f4325
aki matsuno <matsuaki4@gmail.com>
2022-07-24 23:52:45 +0900

    serverからのresponseに応じてArticleを表示できるようにした

--- lib/ArticleRepository.dart
+++ lib/ArticleRepository.dart
@@ -11,16 +11,7 @@ class ArticleRepository {
         await HttpClient().get('localhost', 8080, '/articles/1234');
     HttpClientResponse response = await request.close();
     final stringData = await response.transform(const Utf8Decoder()).join();
-    print(stringData);
 
-    // TODO 上のstringDataをPurseしてArticleを生成する
-    return const Article(
-        id: 'ID',
-        publishDate: '2022/7/16(土) 11:00',
-        title:
-            'ワンキャリアのCTOが登壇するオンラインセミナー「CTOが特別解説！『サービススケールで直面した組織課題とマイクロサービスアーキテクチャ戦略』」が7月21日に開催',
-        description:
-            '翔泳社とCodeZineが主催する開発者向けイベント「Developers Summit 2022&amp;nbsp;Summer（デブサミ2022夏）」が7月21日にオンラインで開催されます。今年は「アジャイル・DevOps時代のプロダクトとエンジニア組織を支える、Developer Productivityへの道」をテーマとした34セッションを提供します。今回は「開発プロセス」のカテゴリからいくつかのセッションをご紹介します。',
-        link: 'http://codezine.jp/article/detail/16206');
+    return Article.fromJson(jsonDecode(stringData));
   }
 }

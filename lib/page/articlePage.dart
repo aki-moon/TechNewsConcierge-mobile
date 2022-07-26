148fdae2b330d40aa25a97b35d181bbc7cb28ef0
aki matsuno <matsuaki4@gmail.com>
2022-07-26 21:21:22 +0900

    ディレクトリ構成整理

--- lib/page/articlePage.dart
+++ lib/page/articlePage.dart
@@ -3,7 +3,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:technewsconciergemobile2/ArticleRepository.dart';
-import 'package:technewsconciergemobile2/article.dart';
+import 'package:technewsconciergemobile2/dto/article.dart';
 
 class ArticlePage extends ConsumerWidget {
   const ArticlePage({Key? key}) : super(key: key);
